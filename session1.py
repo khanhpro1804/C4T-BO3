7918faaec1f27c0d15a0a45b3f03f06139c53aa4
khanhpro1804 <khanhpro1804>
2019-06-09 12:11:30 +0700

    .

---
+++ session1.py
@@ -0,0 +1,10 @@
+# print('Tinh dien tich tam giac')
+# cao=float(input('chieu cao cua tam giac la '))
+# day=float(input('do dai cua day tam giac la '))
+# S=0.5*day*cao
+# print(S)
+
+# print('Tinh dien tich hinh tron')
+# d=float(input('ban kinh hinh tron la '))
+# S=3.14*d
+# print('Dien tich hinh tron la'S)
